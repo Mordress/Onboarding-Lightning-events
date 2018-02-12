b78af1c1293e0f86ca3bc6b96b41d2a215d77e62
user <user@mail.com>
2018-02-12 18:48:18 +0300

    deleted chart

--- aura/campingListItem/campingListItemController.js
+++ aura/campingListItem/campingListItemController.js
@@ -1,13 +1,9 @@
 ({
     packItem: function (component, event, helper) {
-        /*let pack = component.get("v.item");
-        pack.Packed__c = true;
-        component.set("v.item", pack);
-
-        let button = event.getSource();
-        button.set("v.disabled", true);*/
         let newCampingItem = component.get("v.item");
-        newCampingItem.Packed__c = true;
+        if (event.getSource().get("v.name") === "packed") {
+            newCampingItem.Packed__c = true;
+        }
         helper.updateItem(component, newCampingItem);
         component.set("v.item", newCampingItem);
     },
